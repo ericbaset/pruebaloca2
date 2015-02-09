514dccdddf703681d54cfa5a80ff4df58c1bd511
alu20466180k <alu20466180k@ti4-11>
2015-02-09 16:22:11 +0100

    pruebaloca2

---
+++ git/src/git/Factura.java
@@ -0,0 +1,23 @@
+package git;
+	interface imprimiblePDF{
+		String toPDF();
+	}
+	
+public class Factura implements imprimiblePDF{
+		
+	public Factura (){
+		
+	}
+
+	@Override
+	public String toPDF() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+
+
+	
+
+}
